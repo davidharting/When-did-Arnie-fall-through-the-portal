a3c109d575346ec1cdf0dfc3adc959b276e7fc94
David Harting <davideharting@gmail.com>
2019-02-28 21:07:49 -0500

    Calculate date and time!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
 import React from "react"
+import dayjs from "dayjs"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import "./index.css"
 
+// A "couple of days ago" from the first episode, which was Dec 9 2017
+const IT_HAPPENED = dayjs("2017-12-7")
+
 function howLong() {
-  return [2, 4, 25]
+  const today = dayjs()
+  const years = today.diff(IT_HAPPENED, "years")
+  const months = today.diff(IT_HAPPENED, "months") - years * 12
+  const days = today.diff(IT_HAPPENED, "days") - (years * 365 + months * 30)
+
+  return [years, months, days]
 }
 
 function IndexPage() {
